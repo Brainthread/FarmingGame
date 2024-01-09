7ffc0c962b6c11835a8bf02fa5f03b8678d3398c
Kei Duke Bergman <54135223+Brainthread@users.noreply.github.com>
2024-01-09 23:07:45 +0100

    Add textures

--- Scripts/CameraFollow.gd
+++ Scripts/CameraFollow.gd
@@ -1,16 +1,27 @@
 extends Camera2D
 
 @export var target:Node2D
-@export var followSpeed:float
-@export var lookAhead:float
-@export var minPoint:Vector2
-@export var maxPoint:Vector2
+var target_former_position:Vector2 = Vector2()
+var follow_speed:float = 1000
+@export var look_ahead:bool = false
+@export var min_point:Vector2 = Vector2(-9999999, -9999999)
+@export var max_point:Vector2 = Vector2(9999999, 9999999)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	target_former_position = target.position
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _physics_process(delta):
+	var target_heading = target.position - target_former_position
+	var target_velocity = target_heading/delta
+	
+	if look_ahead == true:
+		pass
+	position = position.move_toward(target.position, follow_speed * delta)
+	target_former_position = target.position
--- Scripts/Player/States/IdleAndMove.gd
+++ Scripts/Player/States/IdleAndMove.gd
@@ -1,8 +1,13 @@
 extends State
 
 var _input_direction = Vector2()
+var _input_sprint:bool = false
 var _body:CharacterBody2D
 @export var _movement_speed:float = 10000
+@export var _acceleration:float = 10000
+@export var _sprint_multiplier:float = 2
+@export var _direction_change_modifier = 3
+var _movement_velocity:Vector2 = Vector2()
 
 func _initialize_state(stateMachine_node:FiniteStateMachine, root_node:Node2D):
 	super(stateMachine_node, root_node)
@@ -28,7 +33,19 @@ func _handle_input():
 	_input_direction = Vector2(
 			Input.get_action_strength("Right") - Input.get_action_strength("Left"), 
 			Input.get_action_strength("Down") - Input.get_action_strength("Up"))
+	_input_direction = _input_direction.normalized()
+	_input_sprint = true if Input.get_action_strength("Sprint") else false
+
 
 func _move_character(_delta: float):
-	_body.velocity = _movement_speed * _delta * _input_direction
+	var _direction_change_coefficient = 1
+	var _sprint_coefficent = _sprint_multiplier if _input_sprint else 1
+	if _movement_velocity.dot(_input_direction) < 0 and not _movement_velocity == Vector2.ZERO and not _input_direction == Vector2.ZERO:
+		_direction_change_coefficient = _direction_change_modifier
+	
+	var _target_velocity = _input_direction * _movement_speed * _sprint_coefficent
+	var _point_acceleration = _delta * _acceleration * _direction_change_coefficient
+	_movement_velocity = _movement_velocity.move_toward(_target_velocity, _point_acceleration)
+	
+	_body.velocity = _delta * _movement_velocity
 	_body.move_and_slide()
